45d051a8d8e931f73e3bb2fec7efe16e697991f5
oscar <oscar.llapar@educa.jcyl.es>
2021-10-15 11:05:12 +0200

    github nuevo

---
+++ indexProyectoDWEC.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Desarrollo Web en Entorno Servidor</title>
+        <link href="webroot/css/estilos.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+            <div class="titulo">
+                <h1>Desarrollo Web en Entorno Cliente</h1>
+                <h2>Óscar Llamas Parra</h2>
+                <div class="cuadro" id="arriba"></div>
+            </div>
+        <div class="cont">
+            
+        </div>
+			
+    </body>
+    <footer>
+        <p>
+            Óscar Llamas Parra
+			<a href="https://github.com/OscarLlaPar/" target="__blank"><img src="webroot/img/github.png" alt="Github"></img></a>
+        </p>
+        <p>
+            DAW 2
+        </p>
+        <p>
+            IES Los Sauces, Benavente 2021-2022
+        </p>
+        <div class="cuadro" id="abajo"></div>
+    </footer>
+</html>
